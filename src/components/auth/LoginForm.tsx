90759436a06be3cad7f31a83968185d091d0ebb9
Daliska F. Royan <firstaroyan09@gmail.com>
2024-07-31 00:06:41 +0700

    feat: change banner dimension

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -9,7 +9,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import CardWrapper from './CardWrapper';
-import Image from "next/image"
+import Image from "next/image";
 
 import { signInAction } from '@/app/(auth)/login/actions';
 import { ERROR_MESSAGES } from '@/lib/errors';
@@ -25,7 +25,7 @@ import { z } from 'zod';
 import { useServerAction } from 'zsa-react';
 import { Alert, AlertDescription, AlertTitle } from '../ui/alert';
 import { Button, buttonVariants } from '../ui/button';
-import { Label } from "@/components/ui/label"
+import { Label } from "@/components/ui/label";
 import { Input } from '../ui/input';
 import BackButton from './BackButton';
 import GoogleIcon from './icons/GoogleIcon';
@@ -52,7 +52,7 @@ const LOGIN_FORM_STRINGS = {
 
 const DEFAULT_COUNT = 30;
 
-export const Dashboard = () => {
+export const LoginForm = () => {
   const [isUnverified, setIsUnverified] = useState(false);
   const [count, setCount] = useState(DEFAULT_COUNT);
   const [startCount, setStartCount] = useState(false);
@@ -216,177 +216,18 @@ export const Dashboard = () => {
           </form>
         </Form>
       </div>
-      <div className="hidden bg-muted lg:block">
+      <div className="hidden lg:block">
         <Image
           src="/auth-banner.jpg"
           alt="Image"
           width="1920"
           height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+          className="h-full w-full object-cover"
+          style={{ objectFit: 'cover', overflow: 'hidden' }}
         />
       </div>
     </section>
   )
 }
 
-
-const LoginForm = () => {
-  const [isUnverified, setIsUnverified] = useState(false);
-  const [count, setCount] = useState(DEFAULT_COUNT);
-  const [startCount, setStartCount] = useState(false);
-
-  useEffect(() => {
-    if (!startCount) return;
-    const timer = setInterval(() => {
-      if (count === 0) {
-        setStartCount(false);
-        return clearInterval(timer);
-      }
-      setCount((prevCount) => prevCount - 1);
-      console.log(count);
-    }, 1000);
-
-    return () => {
-      clearInterval(timer);
-    };
-  }, [startCount, count]);
-
-  const { isPending, execute, error } = useServerAction(signInAction, {
-    onError({ err }) {
-      toast.error(err.message);
-      if (
-        err?.message == ERROR_MESSAGES.UnverifiedAccountError ||
-        err?.message ==
-        getDevOnlyErrorMsg(ERROR_MESSAGES.UnverifiedAccountError)
-      ) {
-        setIsUnverified(true);
-        setStartCount(true);
-      }
-    },
-    onSuccess() {
-      toast.success(LOGIN_FORM_STRINGS.successMessage);
-    },
-  });
-
-  const form = useForm({
-    resolver: zodResolver(SignInWithPasswordFormSchema),
-    defaultValues: {
-      email: '',
-      password: '',
-    },
-  });
-
-  const resetUnverified = () => {
-    setIsUnverified(false);
-    setStartCount(false);
-    setCount(DEFAULT_COUNT);
-  };
-
-  async function onSubmit(
-    values: z.infer<typeof SignInWithPasswordFormSchema>
-  ) {
-    resetUnverified();
-    toast.loading(LOGIN_FORM_STRINGS.loadingMessage);
-    await execute(values);
-    toast.dismiss();
-  }
-
-  return (
-    <CardWrapper
-      label={LOGIN_FORM_STRINGS.label}
-      title={LOGIN_FORM_STRINGS.title}
-      backButtonHref='/register'
-      backButtonLabel={LOGIN_FORM_STRINGS.backButtonLabel}>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className='space-y-6'>
-          <div className='space-y-4'>
-            <FormField
-              control={form.control}
-              name='email'
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{LOGIN_FORM_STRINGS.emailFormLabel}</FormLabel>
-                  <FormControl>
-                    <Input
-                      {...field}
-                      type='email'
-                      placeholder='johndoe@gmail.com'
-                    />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={form.control}
-              name='password'
-              render={({ field }) => (
-                <FormItem>
-                  <div className='flex items-center justify-between'>
-                    <FormLabel>
-                      {LOGIN_FORM_STRINGS.passwordFormLabel}
-                    </FormLabel>
-                    <BackButton
-                      isInline
-                      href='/forgot-password'
-                      label={LOGIN_FORM_STRINGS.forgotPasswordLabel}
-                    />
-                  </div>
-
-                  <FormControl>
-                    <Input {...field} type='password' placeholder='******' />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-          </div>
-          {isUnverified && (
-            <div className='text-center'>
-              <p className='text-sm text-muted-foreground'>
-                {LOGIN_FORM_STRINGS.unverifiedMessage}
-              </p>
-              <BackButton
-                href='/email-verification'
-                label={
-                  count > 0
-                    ? `${LOGIN_FORM_STRINGS.resendVerificationCountdown} ${count}s`
-                    : LOGIN_FORM_STRINGS.resendVerificationLabel
-                }
-                isInline
-                disabled={isPending || count > 0}
-              />
-            </div>
-          )}
-          {error && (
-            <Alert variant='destructive'>
-              <Terminal className='h-4 w-4' />
-              <AlertTitle>{LOGIN_FORM_STRINGS.errorMessage}</AlertTitle>
-              <AlertDescription>
-                {error.message || LOGIN_FORM_STRINGS.errorDescription}
-              </AlertDescription>
-            </Alert>
-          )}
-          <Button type='submit' className='w-full' disabled={isPending}>
-            {isPending
-              ? LOGIN_FORM_STRINGS.pendingButton
-              : LOGIN_FORM_STRINGS.submitButton}
-          </Button>
-          <Link
-            href='/api/login/google'
-            className={cn(
-              buttonVariants({
-                variant: 'secondary',
-              }),
-              'w-full'
-            )}>
-            <GoogleIcon className='stroke-white mr-2 h-5 w-5' />
-            {LOGIN_FORM_STRINGS.googleLogin}
-          </Link>
-        </form>
-      </Form>
-    </CardWrapper>
-  );
-};
-
-export default Dashboard;
+export default LoginForm;
